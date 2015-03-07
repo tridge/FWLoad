2437da2868db6f968d12c5cf1c438cd86a3e1bf9
Andrew Tridgell <andrew@tridgell.net>
2015-03-07 18:26:47 +1100

    jtag: added erase and attach

--- jtag.py
+++ jtag.py
@@ -43,8 +43,59 @@ def load_firmware(device, firmware, mcu_id, run=False):
             gdb.send("run\n")
             gdb.expect("Start it from the beginning?")
             gdb.send("y\n")
-    except Exceptions as ex:
+    except Exception as ex:
         util.show_error('Loading firmware %s' % firmware, ex, log)        
+
+def erase_firmware(device, mcu_id):
+    '''erase a firmware'''
+    cmd = GDB
+    print("Erasing firmware for '%s'" % mcu_id)
+    try:
+        gdb = pexpect.spawn(cmd, logfile=sys.stdout, timeout=10)
+        gdb.expect("(gdb)")
+        gdb.send("target extended %s\n" % device)
+        gdb.expect("Remote debugging using")
+        gdb.expect("(gdb)")
+        gdb.send("monitor swdp_scan\n")
+        cpu = gdb.expect([CPUID_IO, CPUID_FMU])
+        if cpu == 0:
+            if mcu_id != CPUID_IO:
+                util.failure("Incorrect CPU ID '%s' - expected '%s'" % (CPUID_IO, mcu_id))
+        else:
+            if mcu_id != CPUID_FMU:
+                util.failure("Incorrect CPU ID '%s' - expected '%s'" % (CPUID_FMU, mcu_id))
+        gdb.expect("(gdb)")
+        gdb.send("attach 1\n")
+        gdb.expect("(gdb)")
+        gdb.send("set mem inaccessible-by-default off\n")
+        gdb.expect("(gdb)")
+        gdb.send("monitor erase\n")
+        gdb.expect("(gdb)", timeout=20)
+    except Exception as ex:
+        util.show_error('Erasing firmware', ex)
+
+def attach_gdb(device, mcu_id):
+    '''attach to gdb'''
+    cmd = GDB
+    gdb = pexpect.spawn(cmd, logfile=None, timeout=10)
+    gdb.expect("(gdb)")
+    gdb.send("target extended %s\n" % device)
+    gdb.expect("Remote debugging using")
+    gdb.expect("(gdb)")
+    gdb.send("monitor swdp_scan\n")
+    cpu = gdb.expect([CPUID_IO, CPUID_FMU])
+    if cpu == 0:
+        if mcu_id != CPUID_IO:
+            util.failure("Incorrect CPU ID '%s' - expected '%s'" % (CPUID_IO, mcu_id))
+    else:
+        if mcu_id != CPUID_FMU:
+            util.failure("Incorrect CPU ID '%s' - expected '%s'" % (CPUID_FMU, mcu_id))
+    gdb.expect("(gdb)")
+    gdb.send("attach 1\n")
+    gdb.expect("(gdb)")
+    gdb.send("set mem inaccessible-by-default off\n")
+    gdb.expect("(gdb)")
+    gdb.interact()
 
 def load_all_firmwares(retries=3):
     '''load 4 firmwares. Return True on success, False on failure'''
@@ -80,6 +131,52 @@ def load_all_firmwares(retries=3):
     print("All firmwares loaded OK")
     return True
 
+def erase_firmwares(retries=3):
+    '''erase both firmwares. Return True on success, False on failure'''
+    while retries > 0:
+        retries -= 1
+        if not util.wait_devices([IO_JTAG, FMU_JTAG, FMU_DEBUG]):
+            if retries == 1:
+                print("RETRIES=1 - POWER CYCLING")
+                power_control.power_cycle(down_time=4)
+            continue
+
+        try:
+            erase_firmware(IO_JTAG, CPUID_IO)
+            erase_firmware(FMU_JTAG, CPUID_FMU)
+        except Exception as ex:
+            print(ex)
+            continue
+
+        if util.wait_devices([USB_DEV_TEST, USB_DEV_REFERENCE]):
+            break
+        if retries > 0:
+            print("RETRIES %u - TRYING AGAIN" % retries)
+    if retries == 0:
+        print("FAILED TO ERASE FIRMWARES")
+        return False
+
+    if not util.wait_devices([USB_DEV_TEST, USB_DEV_REFERENCE]):
+        print("Failed to find USB devices")
+        return False
+
+    print("All firmwares erased OK")
+    return True
+
 
 if __name__ == '__main__':
-    load_all_firmwares()
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description=__doc__)
+
+    parser.add_argument("--erase", default=False, action='store_true', help="erase flash")
+    parser.add_argument("--io", default=False, action='store_true', help="attach gdb to io")
+    parser.add_argument("--fmu", default=False, action='store_true', help="attach gdb to fmu")
+    args = parser.parse_args()
+    if args.io:
+        attach_gdb(IO_JTAG, CPUID_IO)
+    elif args.fmu:
+        attach_gdb(FMU_JTAG, CPUID_FMU)
+    elif args.erase:
+        erase_firmwares()
+    else:
+        load_all_firmwares()

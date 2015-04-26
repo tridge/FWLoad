d9c652ff8e7fcb7f7866eae0b9ea2228e046a204
Andrew Tridgell <andrew@tridgell.net>
2015-04-26 17:31:47 +1000

    added compress_logs.sh

---
+++ scripts/compress_logs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# compress log files ready for transfer to master server
+
+set -e
+shopt -s nullglob
+
+cd logs || exit 1
+for d in 20*; do
+    pushd $d || continue
+    echo "Checking $d"
+    for run in run*; do
+        if test $(find "$run" -name "$run" -type d -mmin +15)
+        then
+            echo "Compressing $d/$run"
+            tar -czf $run.tar.gz $run
+            sync
+            rm -rf $run
+        fi
+    done
+    # remove empty directories that have been transferred
+    popd
+    rmdir --ignore-fail-on-non-empty $d || /bin/true
+done
